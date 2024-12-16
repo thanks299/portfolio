4ae6dba00e34462b8cb913cfeb10c2ba1408daa6
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-12-16 12:14:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Agbeble Thanks - Portfolio',
-  description: 'My Personal portfolio , a Full Stack Developer',
-}
+  description: 'My Personal portfolio, a Full Stack Developer',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <head>
+        {/* Ensures a responsive layout */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body
+        className={`${inter.className} bg-gray-900 text-white min-h-screen overflow-x-hidden`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }

0686c420ddb8fdc68566a2fb8a08280030225e06
thanks299 <139892876+thanks299@users.noreply.github.com>
2025-01-25 20:44:51 +0100

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -9,6 +9,7 @@ export default function Projects({ setActiveSection }: ProjectsProps) {
     { id: 1, title: 'Project 1', description: 'Description of Project 1', image: '/placeholder.svg' },
     { id: 2, title: 'Project 2', description: 'Description of Project 2', image: '/placeholder.svg' },
     { id: 3, title: 'Project 3', description: 'Description of Project 3', image: '/placeholder.svg' },
+    { id: 4, title: 'Project 4', description: 'Description of Project 3', image: '/placeholder.svg' },
   ]
 
   return (
